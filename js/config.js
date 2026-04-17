33c510da93331d8f0eb65099ebfe69a4376b20c1
Sveyger <gugle.miks.mikol@gmail.com>
2026-04-17 14:56:29 +0700

    Add Supabase credentials

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-window.SUPABASE_URL = '';
-window.SUPABASE_ANON_KEY = '';
+window.SUPABASE_URL = 'https://hbzdbigmfawgcvzeljdh.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_F_TudEEfDy-KiO-9RTsMYg_9O2WTNYo';
